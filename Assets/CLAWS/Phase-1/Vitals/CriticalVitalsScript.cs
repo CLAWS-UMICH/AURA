82bf2a63c7a9a1a0e45c497800d2f4f895cde918
Benjamin Swartz <baswartz@umich.edu>
2024-11-13 20:39:16 -0500

    Started critical vitals script

--- Assets/CLAWS/Phase-1/Vitals/CriticalVitalsScript.cs
+++ Assets/CLAWS/Phase-1/Vitals/CriticalVitalsScript.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class CriticalVitalsScript : MonoBehaviour
 {
+    private Subscription<VitalsUpdatedEvent> vitalsUpdateEvent;
+    private Subscription<FellowAstronautVitalsDataChangeEvent> fellowVitalsUpdateEvent;
+    [SerializeField] private GameObject heartRate, oxygenCons, carbonProd, temp;
+    [SerializeField] private GameObject heartRate2, oxygenCons2, carbonProd2, temp2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vitalsUpdateEvent = EventBus.Subscribe<VitalsUpdatedEvent>(onVitalsUpdate);
+        fellowVitalsUpdateEvent = EventBus.Subscribe<FellowAstronautVitalsDataChangeEvent>(onFellowVitalsUpdate);
     }
 
     // Update is called once per frame
