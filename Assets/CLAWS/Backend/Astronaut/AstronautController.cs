40cb268171c1748cca1b53ddac71c9a76a9bd9c5
mjmal01 <143458012+mjmal01@users.noreply.github.com>
2024-11-19 17:24:09 -0500

    Add files via upload

--- Assets/CLAWS/Backend/Astronaut/AstronautController.cs
+++ Assets/CLAWS/Backend/Astronaut/AstronautController.cs
@@ -31,6 +31,38 @@ public class Location
     }
 }
 
+// Vitals
+[System.Serializable]
+public class Vitals
+{
+    public int eva_time;
+    public double batt_time_left;
+    public double oxy_pri_storage;
+    public double oxy_sec_storage;
+    public double oxy_pri_pressure;
+    public double oxy_sec_pressure;
+    public int oxy_time_left;
+    public double heart_rate;
+    public double oxy_consumption;
+    public double co2_production;
+    public double suit_pressure_oxy;
+    public double suit_pressure_co2;
+    public double suit_pressure_other;
+    public double suit_pressure_total;
+    public double fan_pri_rpm;
+    public double fan_sec_rpm;
+    public double helmet_pressure_co2;
+    public double scrubber_a_co2_storage;
+    public double scrubber_b_co2_storage;
+    public double temperature;
+    public double coolant_m;
+    public double coolant_gas_pressure;
+    public double coolant_liquid_pressure;
+
+    public double batt_percentage;
+    public double oxy_percentage;
+}
+
 // TestWebObj
 [System.Serializable]
 public class TestWebObj
