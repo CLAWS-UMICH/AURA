eae776fbe0eea13b28253485dce3cb253b62e053
Benjamin Swartz <baswartz@umich.edu>
2024-11-19 17:42:13 -0500

    Created vitals update for critical vitals

--- Assets/CLAWS/Phase-1/Vitals/CriticalVitalsScript.cs
+++ Assets/CLAWS/Phase-1/Vitals/CriticalVitalsScript.cs
@@ -6,20 +6,36 @@ using System;
 
 public class CriticalVitalsScript : MonoBehaviour
 {
-    private Subscription<VitalsUpdatedEvent> vitalsUpdateEvent;
+    private Subscription<UpdatedVitalsEvent> vitalsUpdateEvent;
     private Subscription<FellowAstronautVitalsDataChangeEvent> fellowVitalsUpdateEvent;
     [SerializeField] private GameObject heartRate, oxygenCons, carbonProd, temp;
     [SerializeField] private GameObject heartRate2, oxygenCons2, carbonProd2, temp2;
     // Start is called before the first frame update
     void Start()
     {
-        vitalsUpdateEvent = EventBus.Subscribe<VitalsUpdatedEvent>(onVitalsUpdate);
+        vitalsUpdateEvent = EventBus.Subscribe<UpdatedVitalsEvent>(onVitalsUpdate);
         fellowVitalsUpdateEvent = EventBus.Subscribe<FellowAstronautVitalsDataChangeEvent>(onFellowVitalsUpdate);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void onVitalsUpdate(UpdatedVitalsEvent e) {
+        // astr1 vitals update
+        oxygenCons.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.oxy_consumption.ToString("F1");
+        heartRate.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.heart_rate.ToString("F0");
+        carbonProd.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.co2_production.ToString("F1");
+        temp.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.temperature.ToString("F0");
+    }
+
+    private void onFellowVitalsUpdate(FellowAstronautVitalsDataChangeEvent e) {
+        // astr2 vitals update
+        oxygenCons2.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.oxy_consumption.ToString("F1");
+        heartRate2.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.heart_rate.ToString("F0");
+        carbonProd2.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.co2_production.ToString("F1");
+        temp2.transform.Find("BodyText").GetComponent<TextMeshPro>().text = e.vitals.temperature.ToString("F0");
+
+    }
+
+    private void OnDestroy() {
+        EventBus.Unsubscribe(vitalsUpdateEvent);
+        EventBus.Unsubscribe(fellowVitalsUpdateEvent);
     }
 }
