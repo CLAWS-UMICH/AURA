993c6d920038dd14170948aa7bbb7b183f10c573
SKF2006 <stevenfu@umich.edu>
2024-11-19 19:20:16 -0500

    created TaskListEvent

--- Assets/CLAWS/Backend/EventSystem/EventTypes.cs
+++ Assets/CLAWS/Backend/EventSystem/EventTypes.cs
@@ -122,6 +122,20 @@ public class WebTestEvent
     }
 }
 
+public class TaskListEvent
+{
+    public TaskListObj testData { get; set; }
+    
+    public string use { get; set; }
+
+    public TaskListEvent(TaskListObj _testData, string _use)
+    {
+        testData = _testData;
+        use = _use;
+    }
+
+}
+
 public class UpdatedVitalsEvent
 {
     public Vitals vitals { get; private set; }
