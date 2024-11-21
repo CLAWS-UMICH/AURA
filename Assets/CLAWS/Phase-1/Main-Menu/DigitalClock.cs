d640898a77660f680ae0ec2f0b2179ba72aa325c
Mughani05 <mghani@umich.edu>
2024-11-20 20:52:36 -0500

    timer, wip background

---
+++ Assets/CLAWS/Phase-1/Main-Menu/DigitalClock.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine;
+
+public class DigitalClock : MonoBehaviour
+{
+    private TextMeshProUGUI textDisplay;
+    string hour, minute, second;
+    DateTime timeNow;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textDisplay = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeNow = DateTime.Now;
+        hour = timeNow.Hour.ToString().PadLeft(2, '0');
+        minute = timeNow.Minute.ToString().PadLeft(2, '0');
+        second = timeNow.Second.ToString().PadLeft(2, '0');
+
+        textDisplay.text = hour + ":" + minute + ":" + second;
+    }
+}
