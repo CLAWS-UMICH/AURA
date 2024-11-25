98d069a79976a483f4cb0c3883753abcbde9ebb6
MacaPeralta <peraltam@umich.edu>
2024-11-25 18:11:20 -0500

    backend data script in-progress

--- Assets/CLAWS/Phase-1/Vitals/SuitHelmetPressure/SHP.cs
+++ Assets/CLAWS/Phase-1/Vitals/SuitHelmetPressure/SHP.cs
@@ -1,4 +1,8 @@
-/*using System.Collections;
+/* 
+ * 
+ * starting backend connection 
+ 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,15 +11,16 @@ using System;
 [Serializable]
 public class SuitVariable
 {
-    public GameObject displayObject;
-    public float max;
-    public float min;
+    public GameObject displayObject; // UI Object to update
+    public float max; // Maximum value of the variable
+    public float min; // Minimum value of the variable
 }
 
 public class SuitsControlController : MonoBehaviour
 {
     private Subscription<VitalsUpdatedEvent> vitalsUpdateEvent;
 
+    // Define the five SuitVariables
     public SuitVariable var_1;
     public SuitVariable var_2;
     public SuitVariable var_3;
@@ -24,11 +29,13 @@ public class SuitsControlController : MonoBehaviour
 
     private void Start()
     {
+        // Subscribe to the vitals update event
         vitalsUpdateEvent = EventBus.Subscribe<VitalsUpdatedEvent>(onVitalsUpdate);
     }
 
     private void onVitalsUpdate(VitalsUpdatedEvent e)
     {
+        // Update each variable with the current value from the event
         UpdateVariable(var_1, e.vitals.value1);
         UpdateVariable(var_2, e.vitals.value2);
         UpdateVariable(var_3, e.vitals.value3);
@@ -38,10 +45,18 @@ public class SuitsControlController : MonoBehaviour
 
     private void UpdateVariable(SuitVariable variable, float currentValue)
     {
+        // Normalize the value to a 0-1 range based on min and max
         float normalizedValue = Mathf.Clamp01((currentValue - variable.min) / (variable.max - variable.min));
+
+        // Calculate the arc value for the ring's display
         float arcValue = (1 - normalizedValue) * 302;
 
-        variable.displayObject.transform.Find("RingFull").GetComponent<SpriteRenderer>().material.SetFloat("_Arc1", arcValue);
-        variable.displayObject.transform.Find("BodyText").GetComponent<TextMeshPro>().text = currentValue.ToString("F1");
+        // Update the visual ring arc to match the current value
+        variable.displayObject.transform.Find("RingFull")
+            .GetComponent<SpriteRenderer>().material.SetFloat("_Arc1", arcValue);
+
+        // Update the text display with the current value formatted to one decimal place
+        variable.displayObject.transform.Find("BodyText")
+            .GetComponent<TextMeshPro>().text = currentValue.ToString("F1");
     }
-}*/
+} */
