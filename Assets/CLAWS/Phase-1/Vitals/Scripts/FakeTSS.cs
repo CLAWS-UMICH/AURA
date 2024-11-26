dd6296b158bbe22075a40e1457dd69c6e2ede490
Molly Maloney <mjmal@umich.edu>
2024-11-26 12:08:45 -0500

    vitals almsot fully done, only need to fix connecting sliders and radial views

--- Assets/CLAWS/Phase-1/Vitals/Scripts/FakeTSS.cs
+++ Assets/CLAWS/Phase-1/Vitals/Scripts/FakeTSS.cs
@@ -24,43 +24,42 @@ public class FakeTSS : MonoBehaviour
         while (true)
         {
             Debug.Log("Test"); 
-            //AstronautInstance.User.VitalsData.eva_time = 69;
+            AstronautInstance.User.vitals.eva_time = 69;
             yield return new WaitForSeconds(secondsToUpdate);
             
             // Update vitals with random values
-            AstronautInstance.User.vitals.co2_production = UnityEngine.Random.Range(0.05f, 0.15f);
-            AstronautInstance.User.vitals.oxy_consumption = UnityEngine.Random.Range(0.05f, 0.15f);
-            //AstronautInstance.User.VitalsData.batt_percentage = UnityEngine.Random.Range(90f, 100f);
-            //AstronautInstance.User.VitalsData.oxy_sec_storage = UnityEngine.Random.Range(90f, 100f);
-            //AstronautInstance.User.VitalsData.suit_pressure = UnityEngine.Random.Range(0.8f, 5.2f);
-            //AstronautInstance.User.VitalsData.sub_pressure = UnityEngine.Random.Range(15f, 25f);
-            //AstronautInstance.User.VitalsData.o2_pressure = UnityEngine.Random.Range(0.8f, 5.2f);
-            //AstronautInstance.User.VitalsData.o2_rate = UnityEngine.Random.Range(50f, 90f);
-            //AstronautInstance.User.VitalsData.h2o_gas_pressure = UnityEngine.Random.Range(8f, 15f);
-            //AstronautInstance.User.VitalsData.h2o_liquid_pressure = UnityEngine.Random.Range(18f, 25f);
-            //AstronautInstance.User.VitalsData.sop_pressure = UnityEngine.Random.Range(6f, 12f);
-            //AstronautInstance.User.VitalsData.sop_rate = UnityEngine.Random.Range(1f, 3f);
-            AstronautInstance.User.vitals.heart_rate = UnityEngine.Random.Range(50f, 160f);
-            //AstronautInstance.User.VitalsData.fan_tachometer = UnityEngine.Random.Range(1000f, 3000f);
-            //AstronautInstance.Usesr.VitalsData.battery_capacity = UnityEngine.Random.Range(3000f, 5000f);
-            AstronautInstance.User.vitals.temperature = UnityEngine.Random.Range(50f, 90f);
-            //AstronautInstance.User.VitalsData.battery_percentage = UnityEngine.Random.Range(70f, 100f);
-            //AstronautInstance.User.VitalsData.battery_outputput = UnityEngine.Random.Range(200f, 400f);
-            //AstronautInstance.User.VitalsData.oxygen_primary_time = UnityEngine.Random.Range(1000f, 2000f);
-            //AstronautInstance.User.VitalsData.oxygen_secondary_time = UnityEngine.Random.Range(1500f, 2500f);
-            //AstronautInstance.User.VitalsData.water_capacity = UnityEngine.Random.Range(2000f, 4000f);
+            AstronautInstance.User.vitals.co2_production = UnityEngine.Random.Range(0f, 0.2f);
+            AstronautInstance.User.vitals.oxy_consumption = UnityEngine.Random.Range(0f, 0.2f);
+            AstronautInstance.User.vitals.heart_rate = UnityEngine.Random.Range(20f, 200f);
+            AstronautInstance.User.vitals.temperature = UnityEngine.Random.Range(30f, 100f);
 
-            // Increment timer
-            timer += 3f;
+            AstronautInstance.User.vitals.helmet_pressure_co2 = UnityEngine.Random.Range(0f, 0.2f);
+            AstronautInstance.User.vitals.suit_pressure_co2 = UnityEngine.Random.Range(0f, 0.1f);
+            AstronautInstance.User.vitals.suit_pressure_oxy = UnityEngine.Random.Range(3.5f, 4.7f);
+            AstronautInstance.User.vitals.suit_pressure_total = UnityEngine.Random.Range(3.5f, 4.7f);
+            AstronautInstance.User.vitals.suit_pressure_other = UnityEngine.Random.Range(0f, 0.7f);
 
-            // Battery time left
-            //TimeSpan batteryTime = TimeSpan.FromMinutes(5) - timeSpan;
-            //AstronautInstance.User.VitalsData.batt_time_left = batteryTime.Hours + batteryTime.Minutes + batteryTime.Seconds;
+            AstronautInstance.User.vitals.oxy_pri_storage = UnityEngine.Random.Range(0f, 100f);            
+            AstronautInstance.User.vitals.oxy_sec_storage = UnityEngine.Random.Range(0f, 100f);
+            AstronautInstance.User.vitals.oxy_pri_pressure = UnityEngine.Random.Range(400f, 3200f);
+            AstronautInstance.User.vitals.oxy_sec_pressure = UnityEngine.Random.Range(400f, 3200f);
+            AstronautInstance.User.vitals.coolant_m = UnityEngine.Random.Range(70f, 100f);
+        
+            AstronautInstance.User.vitals.fan_pri_rpm = UnityEngine.Random.Range(19000f, 32000f);
+            AstronautInstance.User.vitals.fan_sec_rpm = UnityEngine.Random.Range(19000f, 32000f);
+            AstronautInstance.User.vitals.scrubber_a_co2_storage = UnityEngine.Random.Range(0f, 70f);
+            AstronautInstance.User.vitals.scrubber_b_co2_storage = UnityEngine.Random.Range(0f, 70f);
+            AstronautInstance.User.vitals.coolant_liquid_pressure = UnityEngine.Random.Range(80f, 750f);
+            AstronautInstance.User.vitals.coolant_gas_pressure = UnityEngine.Random.Range(80f, 750f);
 
-            // O2 time left
-            //TimeSpan o2Time = TimeSpan.FromMinutes(5) - timeSpan;
-            //AstronautInstance.User.VitalsData.oxy_time_left = o2Time.Hours + o2Time.Minutes + o2Time.Seconds;
+             timer += secondsToUpdate;
 
+            // Calculate time left for battery and oxygen
+            TimeSpan batteryTime = TimeSpan.FromMinutes(5) - TimeSpan.FromSeconds(timer);
+            TimeSpan o2Time = TimeSpan.FromMinutes(5) - TimeSpan.FromSeconds(timer);
+
+            AstronautInstance.User.vitals.batt_time_left = (int)batteryTime.TotalMinutes;
+            AstronautInstance.User.vitals.oxy_time_left = (int)o2Time.TotalMinutes;
 
             // Publish VitalsUpdatedEvent
             EventBus.Publish<UpdatedVitalsEvent>(new UpdatedVitalsEvent(AstronautInstance.User.vitals));
