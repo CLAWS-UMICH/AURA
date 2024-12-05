5cd81698b6c01545a64d7bb977fa2cfdff493223
John <57152121+JohnRogan@users.noreply.github.com>
2024-12-04 21:03:37 -0500

    backend further work

--- Assets/CLAWS/Backend/Astronaut/AstronautController.cs
+++ Assets/CLAWS/Backend/Astronaut/AstronautController.cs
@@ -83,58 +83,7 @@ public class TestWebObj
 [System.Serializable]
 public class TasklistObj
 {
-    public class TaskObj
-    {
-        public int task_id;
-        public int status;
-        public string title;
-        public string taskType;
-        public string description;
-        public bool isEmergency;
-        public bool isShared;
-        public bool isSubtask;
-
-        // change later for location constructor if sent in tuple
-        public string location;
-        public List<int> astronauts;
-        public List<TaskObj> subtasks;
-        public TaskObj()
-        {
-            task_id = 0;
-            status = 0;
-            title = "";
-            description = "";
-            isEmergency = false;
-            isShared = false;
-            isSubtask = false;
-            location = "";
-            astronauts = new List<int>();
-            subtasks = new List<TaskObj>();
-        }
-        public TaskObj(int t_id, int st, string tle, string desc, bool em, bool sh, bool sut, string loc, List<int> astrs, List<TaskObj> subts)
-        {
-            task_id = t_id;
-            status = st;
-            title = tle;
-            description = desc;
-            isEmergency = em;
-            isShared = sh;
-            isSubtask = sut;
-            location = loc;
-            astronauts = new List<int>();
-            foreach(int a in astrs)
-            {
-                astronauts.Add(a);
-            }
-            subtasks = new List<TaskObj>();
-            if (description.Length > 0){
-                foreach (TaskObj t in subts)
-                {
-                    subtasks.Add(t);
-                }
-            }
-        }
-    }
+ 
     List<TaskObj> Tasklist = new List<TaskObj>();
 
     public TasklistObj()
@@ -145,8 +94,62 @@ public class TasklistObj
     {
         foreach (TaskObj task_d in data)
         {
-            TaskObj task = new TaskObj(task_d.task_id, task_d.status, task_d.title, task_d.description, task_d.isEmergency, task_d.isShared, task_d.location, task_d.astronauts, task_d.subtasks);
+            TaskObj task = new TaskObj(task_d.task_id, task_d.status, task_d.title, task_d.taskType, task_d.description, task_d.isEmergency, task_d.isShared, task_d.isSubtask, task_d.location, task_d.astronauts, task_d.subtasks);
             Tasklist.Add(task);
+        }
+    }
+}
+
+public class TaskObj
+{
+    public int task_id;
+    public int status;
+    public string title;
+    public string taskType;
+    public string description;
+    public bool isEmergency;
+    public bool isShared;
+    public bool isSubtask;
+
+    // change later for location constructor if sent in tuple
+    public string location;
+    public List<int> astronauts;
+    public List<TaskObj> subtasks;
+    public TaskObj()
+    {
+        task_id = 0;
+        status = 0;
+        title = "";
+        description = "";
+        isEmergency = false;
+        isShared = false;
+        isSubtask = false;
+        location = "";
+        astronauts = new List<int>();
+        subtasks = new List<TaskObj>();
+    }
+    public TaskObj(int t_id, int st, string tle, string desc, string t_type, bool em, bool sh, bool sut, string loc, List<int> astrs, List<TaskObj> subts)
+    {
+        task_id = t_id;
+        status = st;
+        title = tle;
+        description = desc;
+        isEmergency = em;
+        isShared = sh;
+        isSubtask = sut;
+        location = loc;
+        astronauts = new List<int>();
+        foreach (int a in astrs)
+        {
+            astronauts.Add(a);
+        }
+        subtasks = new List<TaskObj>();
+        if (description.Length > 0)
+        {
+            foreach (TaskObj t in subts)
+            {
+                subtasks.Add(t);
+            }
         }
     }
 }
--- Assets/CLAWS/Phase-1/Tasklist/Scripts/TasklistBackend.cs
+++ Assets/CLAWS/Phase-1/Tasklist/Scripts/TasklistBackend.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class TasklistBackend : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static TasklistObj tasklist;
+
+    public static TaskObj currentTask;
+
+    private Subscription<TasklistEvent> taskListEvent;
     void Start()
     {
-        
+        taskListEvent = EventBus.Subscribe<TasklistEvent>(SetCurrentTask);
+    }
+
+    public void SetCurrentTask<T> (T e)
+    {
+        eventUse = taskListEvent.use 
     }
 
     // Update is called once per frame
