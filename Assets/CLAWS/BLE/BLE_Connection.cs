99bbadd6988c6cdcfac7ff6b6fb8f2ad7decb361
John <57152121+JohnRogan@users.noreply.github.com>
2025-01-30 13:28:58 -0500

    created script

---
+++ Assets/CLAWS/BLE/BLE_Connection.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+public class BLE_Connection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
