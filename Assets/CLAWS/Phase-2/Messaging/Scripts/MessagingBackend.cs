b7ab34574934375213e43b84078d7033d4f661fd
Molly Maloney <mjmal@umich.edu>
2025-01-31 14:12:13 -0500

    connecting buttons

--- Assets/CLAWS/Phase-2/Messaging/Scripts/MessagingBackend.cs
+++ Assets/CLAWS/Phase-2/Messaging/Scripts/MessagingBackend.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.ARFoundation.VisualScripting;
 
 public class MessagingBackend : MonoBehaviour
 {
     Messaging msgList;
-    List<Message> allMessage = new List<Message>();
+    public List<Message> allMessage = new List<Message>();
     List<Message> AstroChat = new List<Message>();
     List<Message> LMCCChat = new List<Message>();
     List<Message> GroupChat = new List<Message>();
-
-    [SerializeField]private GameObject messageObject;
-    [SerializeField]private GameObject LMCCgc;
-    [SerializeField]private GameObject A2gc;
-    [SerializeField]private GameObject A2andLMCCgc;
-    [SerializeField]private Sprite thumbsUp;
-    [SerializeField]private Sprite thumbsDown;
-    [SerializeField]private Sprite warning;
-    private string messageText;
-    
-
     private Subscription<MessagesAddedEvent> messageAddedEvent;
     private Subscription<MessageSentEvent> messageSentEvent;
     private Subscription<MessageReactionEvent> messageReactionEvent;
     private WebSocketClient webSocketClient;
+
+
     void Start()
     {
         msgList = new Messaging();
@@ -53,146 +45,6 @@ public class MessagingBackend : MonoBehaviour
         }
     }
 
-
-    void sendMessageButton() 
-    {
-        messageText = messageObject.transform.Find("Message").GetComponent<TextMeshPro>().text;
-        int messageCount = allMessage.FindAll(m => m.from == (AstronautInstance.User.id + 1)).Count + 1;
-        Message newMessage;
-        if (LMCCgc.activeSelf)
-        {
-            newMessage = new Message
-            {
-                message_id = messageCount,
-                sent_to = 3,
-                message = messageText,
-                from = AstronautInstance.User.id + 1
-            };
-            EventBus.Publish(new MessageSentEvent(newMessage));
-        }
-        else if (A2gc.activeSelf)
-        {
-            if (AstronautInstance.User.id == 0) 
-            {
-                newMessage = new Message
-                {
-                    message_id = messageCount,
-                    sent_to = 2,
-                    message = messageText,
-                    from = AstronautInstance.User.id + 1
-                };
-                EventBus.Publish(new MessageSentEvent(newMessage));
-            }
-            if (AstronautInstance.User.id == 1)
-            {
-                newMessage = new Message
-                {
-                    message_id = messageCount,
-                    sent_to = 1,
-                    message = messageText,
-                    from = AstronautInstance.User.id + 1
-                };
-                EventBus.Publish(new MessageSentEvent(newMessage));
-            }
-        }
-        else 
-        {
-            newMessage = new Message
-            {
-                message_id = messageCount,
-                sent_to = 4,
-                message = messageText,
-                from = AstronautInstance.User.id + 1
-            };
-            EventBus.Publish(new MessageSentEvent(newMessage));
-        }
-    }
-
-
-    void deleteButton()
-    {
-        messageText = messageObject.transform.Find("Message").GetComponent<TextMeshPro>().text;
-        messageText = "";
-    }
-
-
-    void sendReaction(string JSONreaction)
-    {
-        string messageText = JSONreaction;
-        int messageCount = allMessage.FindAll(m => m.from == (AstronautInstance.User.id + 1)).Count + 1;
-        Message newMessage;
-        if (LMCCgc.activeSelf)
-        {
-            newMessage = new Message
-            {
-                message_id = messageCount,
-                sent_to = 3,
-                message = messageText,
-                from = AstronautInstance.User.id + 1
-            };
-            EventBus.Publish(new MessageSentEvent(newMessage));
-        }
-        else if (A2gc.activeSelf)
-        {
-            if (AstronautInstance.User.id == 0) 
-            {
-                newMessage = new Message
-                {
-                    message_id = messageCount,
-                    sent_to = 2,
-                    message = messageText,
-                    from = AstronautInstance.User.id + 1
-                };
-                EventBus.Publish(new MessageSentEvent(newMessage));
-            }
-            if (AstronautInstance.User.id == 1)
-            {
-                newMessage = new Message
-                {
-                    message_id = messageCount,
-                    sent_to = 1,
-                    message = messageText,
-                    from = AstronautInstance.User.id + 1
-                };
-                EventBus.Publish(new MessageSentEvent(newMessage));
-            }
-        }
-        else 
-        {
-            newMessage = new Message
-            {
-                message_id = messageCount,
-                sent_to = 4,
-                message = messageText,
-                from = AstronautInstance.User.id + 1
-            };
-            EventBus.Publish(new MessageSentEvent(newMessage));
-        }
-    }
-
-
-    void sendThumbsUp()
-    {
-        ImageMessage encodedImage = new ImageMessage(thumbsUp);
-        string jsonString = JsonUtility.ToJson(encodedImage);
-        sendReaction(jsonString);
-    }
-
-
-    void sendThumbsDown()
-    {
-        ImageMessage encodedImage = new ImageMessage(thumbsDown);
-        string jsonString = JsonUtility.ToJson(encodedImage);
-        sendReaction(jsonString);
-    }
-
-
-    void sendWarning()
-    {
-        ImageMessage encodedImage = new ImageMessage(warning);
-        string jsonString = JsonUtility.ToJson(encodedImage);
-        sendReaction(jsonString);
-    }
 
 
     void appendList(MessagesAddedEvent e)
@@ -234,6 +86,9 @@ public class MessagingBackend : MonoBehaviour
         webSocketClient.SendJsonData(json, "MESSAGING");
         // Debug.Log(json);
     }
+
+    
+    
 
 
     void OnDestroy()
