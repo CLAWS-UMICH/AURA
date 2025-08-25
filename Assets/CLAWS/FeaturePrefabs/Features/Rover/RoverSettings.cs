ba3a1c105a9a411df062b2688a18919e6c0c3043
Molly Maloney <mjmal@umich.edu>
2025-08-25 07:22:00 -0400

    Build ready for festifall

--- Assets/CLAWS/FeaturePrefabs/Features/Rover/RoverSettings.cs
+++ Assets/CLAWS/FeaturePrefabs/Features/Rover/RoverSettings.cs
@@ -14,30 +14,8 @@ public class RoverSettings : MonoBehaviour
     public GameObject samplingToggle;
     [Header("Map")]
     public GameObject map;
-    private Subscription<PR_LocationUpdatedEvent> pr_LocationUpdatedEventSubscription;
-    protected Subscription<prUpdatedVitalsEvent> pr_VitalsUpdatedEventSubscription;
 
 
-    void Start()
-    {
-        pr_LocationUpdatedEventSubscription = EventBus.Subscribe<PR_LocationUpdatedEvent>(OnPRLocationUpdated);
-        pr_VitalsUpdatedEventSubscription = EventBus.Subscribe<prUpdatedVitalsEvent>(OnPRVitalsUpdated);
-
-    }
-
-
-    public void OnPRLocationUpdated(PR_LocationUpdatedEvent e)
-    {
-        imu_pos.transform.Find("Title (2)").GetComponent<TMPro.TextMeshProUGUI>().text = "[" + e.data.posX + ", " + e.data.posZ + "]";
-    }
-
-    public void OnPRVitalsUpdated(prUpdatedVitalsEvent e)
-    {
-        //GameObject o2_tank = PR_o2Tank.transform.Find("O2Tank").gameObject;
-        //GameObject battery_level = PR_batteryLevel.transform.Find("BatteryLevel").gameObject;
-        //o2_tank.transform.Find("Value").GetComponent<TMPro.TextMeshProUGUI>().text = e.data.oxygen_tank;
-        //battery_level.transform.Find("Value").GetComponent<TMPro.TextMeshProUGUI>().text = e.data.battery_level;
-    }
 
     public void openFeatureScreen()
     {
